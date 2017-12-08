7575daaa1f2c81ebcee4fafe705d01a10a49bd1c
Hassan <hassan.shehawi@gmail.com>
2017-12-08 17:20:05 +0200

    Basics, talker and listener

---
+++ build/hello_world/CMakeFiles/listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/listener.dir/src/listener.cpp.o"
+  "/home/hassan/ROS-ws/devel/lib/hello_world/listener.pdb"
+  "/home/hassan/ROS-ws/devel/lib/hello_world/listener"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/hello_world/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+  "/home/hassan/ROS-ws/devel/lib/hello_world/talker.pdb"
+  "/home/hassan/ROS-ws/devel/lib/hello_world/talker"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
